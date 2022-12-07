54ef64c9fd114dde810ebffe94b81abb51350f4b
LeoBalleis <leonel.balleis@gmail.com>
2022-12-07 18:41:09 -0300

    yacasi

---
+++ app/Etl.py
@@ -0,0 +1,87 @@
+#DF AMAZON 
+import pandas as pd
+amazon_titles = pd.read_csv("/app/datasets/amazon_prime_titles.csv",delimiter = ',',encoding = "utf-8")
+amazon_titles.drop(columns=['show_id','director','country','rating','description','date_added'], inplace=True)
+amazon_titles.fillna( 'SinDatos',inplace=True)
+amazon_titles = amazon_titles.assign(Plataforma="amazon")
+
+# FALTAN CAMBIAR LAS RUTAS
+
+#DF DISNEY
+
+disney_plus_titles = pd.read_csv("/app/datasets/disney_plus_titles.csv",delimiter = ',',encoding = "utf-8")
+disney_plus_titles.drop(columns=['show_id','director','country','rating','description','date_added'], inplace=True)
+disney_plus_titles.fillna( 'SinDatos',inplace=True)
+disney_plus_titles=disney_plus_titles.assign(Plataforma="disney")
+
+#DF Hulu
+
+hulu_titles=pd.read_csv("/app/datasets/hulu_titles.csv",delimiter = ',',encoding = "utf-8")
+hulu_titles.drop(columns=['show_id','director','country','rating','description','date_added'], inplace=True)
+hulu_titles.fillna( 'SinDatos',inplace=True)
+hulu_titles=hulu_titles.assign(Plataforma="hulu")
+
+#DF Netflix
+
+netflix_titles=pd.read_json("/app/datasets/netflix_titles.json")
+netflix_titles.drop(columns=['show_id','director','country','rating','description','date_added'], inplace=True)
+netflix_titles.fillna( 'SinDatos',inplace=True)
+netflix_titles=netflix_titles.assign(Plataforma="netflix")
+
+# Concatenamos las 4 tablas en un unico df
+
+df_completo= pd.concat([netflix_titles,hulu_titles,disney_plus_titles,amazon_titles])
+df_completo.reset_index(inplace=True)
+# Separo mi columna 'duration' en 'duration' y 'unit'
+dfextra=df_completo.duration.str.split(expand=True)
+dfextra=dfextra.reset_index()
+dfextra[1]=dfextra[1].replace({'Season': 'Seasons'})# Normalizo el str seasons
+df_completo=pd.concat([df_completo,dfextra],axis=1)# concateno con el df principal
+df_completo.drop(columns=['duration','index'],inplace=True)# dropeo las columnas sobrantes
+df_completo=df_completo.rename(columns={0:'duration',1:'unit'})
+df_completo['duration']=df_completo['duration'].replace({'SinDatos': 0})
+df_completo['unit']=df_completo['unit'].replace({None: 0})
+df_completo['duration']=df_completo['duration'].astype(int)
+df_completo['cast']=df_completo['cast'].replace({'SinDatos':None})
+
+# Armo las Querrys para que me devuelvan diccionarios y se me facilite el trabajo con la API
+
+#querry 1 Máxima duración según tipo de film (película/serie), por plataforma y por año: El request debe ser: get_max_duration(año, plataforma, [min o season])
+def get_max_duration(ano, plataforma, min_o_season):
+  dftest=df_completo.query(f'release_year == {ano} and Plataforma == {plataforma} and unit== {min_o_season}').sort_values(by=['duration'],ascending=False)
+  respuesta=dftest[['title','duration','unit']].iloc[0]
+  respuesta=respuesta.to_dict()
+  return respuesta
+
+#querry 2 Cantidad de películas y series (separado) por plataforma El request debe ser: get_count_plataform(plataforma)
+
+def get_count_plataform(plataforma):
+   dfq2=df_completo.query(f'Plataforma == {plataforma}')
+   variable02=dfq2['type'].value_counts()
+   variable02=variable02.to_dict()
+   variable02['Plataforma']= plataforma
+   return variable02
+
+#Querrry 3  Cantidad de veces que se repite un género y plataforma con mayor frecuencia del mismo. El request debe ser: get_listedin('genero')
+# Como ejemplo de género pueden usar 'comedy', el cuál deberia devolverles un cunt de 2099 para la plataforma de amazon.
+
+def get_listedin(genero):
+    df03=df_completo.query (f'listed_in.str.contains({genero})')
+    dtftest=df03[['Plataforma','listed_in']].groupby(['Plataforma']).count()
+    nombre=dtftest.iloc[0].name
+    cantidad=dtftest['listed_in'].iloc[0]
+    diccionario={'plataform':nombre,'cantidad':cantidad}
+    return diccionario
+
+
+##4 Actor que más se repite según plataforma y año. El request debe ser: get_actor(plataforma, año)
+
+def get_actor(plataforma,ano):
+    df04=df_completo.query(f'release_year == {ano} and Plataforma == {plataforma}' )
+    new_df = pd.DataFrame(df04["cast"].str.split(',', expand=True).stack(), columns=["cast"])
+    resultado=new_df.groupby(["cast"])["cast"].count().sort_values(ascending=False)
+    dfx=pd.DataFrame(resultado)
+    nombre=dfx.iloc[0].name
+    cantidad=dfx['cast'].iloc[0]
+    diccionario={'plataform': plataforma,'cantidad': cantidad,'actores':nombre}
+    return diccionario
--- app/main.py
+++ app/main.py
@@ -1,12 +1,73 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
+from Etl import df_completo
+import pandas as pd
 app=FastAPI()
+@app.get("/",response_class=HTMLResponse)
+async def index():
+    return """<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto Individual N°1 - Data Engineering</title>
+</head>
+<body>
+    <h1>Guía de Usuario de la API</h1>
+    <h3>/get_max_duration/AÑO/PLATAFORMA/TIPO Por ej: /get_max_duration/2020/"netflix"/"min"</h3>
+    <p>Devuelve la película/serie con mayor duración por plataforma, año y tipo de duración (min o seasons).</p>
+    <h3>/get_count_platform/PLATAFORMA Por ej: /get_count_platform/disney</h3>
+    <p>Devuelve la cantidad de películas y de series por plataforma</p>
+    <h3>/get_listedin/GENERO Por ej: /get_listedin/comedy</h3>
+    <p>Devuelve la cantidad de veces que se repite un género y plataforma con mayor frecuencia del mismo.</p>
+    <h3>/get_actor/PLATAFORMA/AÑO Por ej: /get_actor/"amazon"/2020</h3>
+    <p>Devuelve al actor/actriz con mayor número de apariciones según año y plataforma.</p>
+    <h3>Luego de realizar alguna consulta, si desea volver a esta guía, elimine los decoradores.</h3>
+</body>
+</html>"""
 
-@app.get("/")
-def read_root():
-    return {'Máxima duración según tipo de film get_max_duration(año, plataforma, [min o season])\n Cantidad de películas y series (separado) por plataforma: get_count_plataform(plataforma)\n Cantidad de veces que se repite un género y plataforma con mayor frecuencia del mismo:get_listedin(genero)\n Actor que más se repite según plataforma y año: get_actor(plataforma, año) '
-    }
 
-@app.get("/user/{user_id}{}")
-def mensaje(user_id):
-    return f"El id del ususrio es: {user_id}"
+
+
+@app.get("/get_max_duration/{ano}/{plataforma}/{tipo}")
+def get_max_duration(ano, plataforma, min_o_season):
+  dftest=df_completo.query(f'release_year == {ano} and Plataforma == {plataforma} and unit== {min_o_season}').sort_values(by=['duration'],ascending=False)
+  respuesta=dftest[['title','duration','unit']].iloc[0]
+  respuesta=respuesta.to_dict()
+  return respuesta
+
+
+@app.get("/get_count_platform/{plataforma}")
+def get_count_plataform(plataforma):
+   dfq2=df_completo.query(f'Plataforma == {plataforma}')
+   variable02=dfq2['type'].value_counts()
+   variable02=variable02.to_dict()
+   variable02['Plataforma']= plataforma
+   return variable02
+
+
+@app.get("/get_listedin/{genero}")
+def get_listedin(genero):
+    df03=df_completo.query (f'listed_in.str.contains({genero})')
+    dtftest=df03[['Plataforma','listed_in']].groupby(['Plataforma']).count()
+    nombre=dtftest.iloc[0].name
+    cantidad=dtftest['listed_in'].iloc[0]
+    diccionario={'plataform':nombre,'cantidad':cantidad}
+    return diccionario
+
+@app.get("/get_actor/{plataforma}/{ano}")
+def get_actor(plataforma,ano):
+    df04=df_completo.query(f'release_year == {ano} and Plataforma == {plataforma}' )
+    new_df = pd.DataFrame(df04["cast"].str.split(',', expand=True).stack(), columns=["cast"])
+    resultado=new_df.groupby(["cast"])["cast"].count().sort_values(ascending=False)
+    dfx=pd.DataFrame(resultado)
+    nombre=dfx.iloc[0].name
+    cantidad=dfx['cast'].iloc[0]
+    diccionario={'plataform': plataforma,'cantidad': cantidad,'actores':nombre}
+    return diccionario
+
+
+
+
+
+
